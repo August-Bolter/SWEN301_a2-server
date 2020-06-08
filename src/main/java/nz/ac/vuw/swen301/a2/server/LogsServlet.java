669760547bead46713286fae1b3d96851c881e8e
August Bolter <august.bolter@gmail.com>
2020-06-08 20:42:16 +1200

    Finished doPost for LogsServlet
    Signed-off-by: August Bolter <august.bolter@gmail.com>

--- src/main/java/nz/ac/vuw/swen301/a2/server/LogsServlet.java
+++ src/main/java/nz/ac/vuw/swen301/a2/server/LogsServlet.java
@@ -14,7 +14,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Locale;
+import java.util.regex.Pattern;
 
 public class LogsServlet extends HttpServlet {
 
@@ -34,10 +34,10 @@ public class LogsServlet extends HttpServlet {
             return;
         }
 
-        int limit = 0;
+        int limit;
         try {
             limit = Integer.parseInt(req.getParameter("limit"));
-            if (limit > Integer.MAX_VALUE || limit < 0) {
+            if (limit < 0) {
                 resp.sendError(400);
                 return;
             }
@@ -74,7 +74,7 @@ public class LogsServlet extends HttpServlet {
 
         for (JsonObject j : logs) {
             for (String s : levelsToConsider) {
-                if(j.get("level").equals(s)) {
+                if(req.getParameter("level").equals(s)) {
                     if (numLogs != limit) {
                         output.add(j);
                         numLogs++;
@@ -99,7 +99,7 @@ public class LogsServlet extends HttpServlet {
             out.println(j.toString());
         }
         out.close();
-        resp.sendError(200);
+        resp.setStatus(200);
     }
 
     @Override
@@ -110,18 +110,125 @@ public class LogsServlet extends HttpServlet {
         String LogEvent = req.getParameter("LogEvent");
         Gson g = new Gson();
         JsonObject j = g.fromJson(LogEvent, JsonObject.class);
+        boolean noErrorDetails;
+        noErrorDetails = j.get("errorDetails") == null;
         if (j.get("id") == null || j.get("message") == null || j.get("timestamp") == null || j.get("thread") == null
         || j.get("logger") == null || j.get("level") == null) {
             resp.sendError(400);
             return;
         }
-        if (j.get("id").equals("") || j.get("message").equals("") || j.get("timestamp").equals("") || j.get("thread").equals("")
-                || j.get("logger").equals("") || j.get("level").equals("")) {
-
-        }
-        if (j.entrySet().size() != 6) {
+        if (j.get("id").isJsonNull() || j.get("message").isJsonNull() || j.get("timestamp").isJsonNull() || j.get("thread").isJsonNull()
+                || j.get("logger").isJsonNull() || j.get("level").isJsonNull()) {
             resp.sendError(400);
+            return;
+        }
+        if (noErrorDetails) {
+            if (j.entrySet().size() != 6) {
+                resp.sendError(400);
+                return;
+            }
+        }
+        else {
+            if (j.entrySet().size() != 7) {
+                resp.sendError(400);
+                return;
+            }
         }
 
+        if (j.get("id").isJsonPrimitive()) {
+            if (j.getAsJsonPrimitive().isString()) {
+                Pattern UUID = Pattern.compile("[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}");
+                if(!UUID.matcher(j.get("id").toString()).matches()) {
+                    resp.sendError(400);
+                    return;
+                }
+                else {
+                    if (logs.size() > 0) {
+                        for (JsonObject jObj : logs) {
+                            if(jObj.get("id").equals(j.get("id"))) {
+                                resp.sendError(409);
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+            else {
+                resp.sendError(400);
+                return;
+            }
+        }
+        else {
+            resp.sendError(400);
+            return;
+        }
+
+        if (j.get("message").isJsonPrimitive() && j.get("thread").isJsonPrimitive() && j.get("logger").isJsonPrimitive()) {
+            if (!j.get("message").getAsJsonPrimitive().isString() || !j.get("thread").getAsJsonPrimitive().isString() || !j.get("logger").getAsJsonPrimitive().isString()) {
+                resp.sendError(400);
+                return;
+            }
+        }
+        else {
+            resp.sendError(400);
+            return;
+        }
+
+        if (j.get("timestamp").isJsonPrimitive()) {
+            if (j.getAsJsonPrimitive().isString()) {
+                if(!j.get("timestamp").toString().matches("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")) {
+                    resp.sendError(400);
+                    return;
+                }
+            }
+            else {
+                resp.sendError(400);
+                return;
+            }
+        }
+        else {
+            resp.sendError(400);
+            return;
+        }
+
+        boolean isValid = true;
+        if (j.get("timestamp").isJsonPrimitive()) {
+            if (j.getAsJsonPrimitive().isString()) {
+                for (levels l : levels.values()) {
+                    if(j.get("level").toString().equals(l.name())) {
+                        isValid = true;
+                        break;
+                    }
+                    isValid = false;
+                }
+                if (!isValid) {
+                    resp.sendError(400);
+                    return;
+                }
+            }
+            else {
+                resp.sendError(400);
+                return;
+            }
+        }
+        else {
+            resp.sendError(400);
+            return;
+        }
+
+        if (!noErrorDetails) {
+            if (j.get("errorDetails").isJsonPrimitive()) {
+                if (!j.getAsJsonPrimitive().isString()) {
+                    resp.sendError(400);
+                    return;
+                }
+            }
+            else {
+                resp.sendError(400);
+                return;
+            }
+        }
+        logs.add(j);
+        resp.setStatus(201);
     }
 }
