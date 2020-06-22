518aebeefd5abf352a72f28cea28c025c290eaa2
August Bolter <august.bolter@gmail.com>
2020-06-22 21:51:28 +1200

    Code commented doGet() and fixed bugs like CSV table being bigger than it should. doGet() still needs to be tested.
    Signed-off-by: August Bolter <august.bolter@gmail.com>

--- src/main/java/nz/ac/vuw/swen301/a2/server/StatsCSVServlet.java
+++ src/main/java/nz/ac/vuw/swen301/a2/server/StatsCSVServlet.java
@@ -23,105 +23,146 @@ public class StatsCSVServlet extends HttpServlet {
         if (LogsServlet.logs.size() > 0) {
             resp.setContentType("text/csv"); //Since output will be CSV text
             PrintWriter writer = resp.getWriter();
-            ArrayList<String> dates = new ArrayList<String>();
-            ArrayList<String> rows = new ArrayList<String>();
-            String[][] results = new String[LogsServlet.logs.size()+1][LogsServlet.logs.size()+1];
+            ArrayList<String> dates = new ArrayList<String>(); //List of log dates
+            ArrayList<String> rows = new ArrayList<String>(); //List of log values (logger, level and thread)
+            String[][] results = new String[LogsServlet.logs.size()+1][LogsServlet.logs.size()+1]; //List of CSV table values (number of log values (logger, thread, level) that match date)
             results[0][0] = "name\t";
 
-            List<JsonObject> logs = new ArrayList<JsonObject>(LogsServlet.logs);
-            Collections.reverse(logs);
+            List<JsonObject> logs = new ArrayList<JsonObject>(LogsServlet.logs); //Deep copy of logs
+            Collections.reverse(logs); //So they can be reversed without affecting original list
 
-            int index = 1;
-            int logIndex = 0;
+            /* Adding the timestamp to the list */
             for (JsonObject log : logs) {
-                if (logIndex+1 == log.size()) {
-                    results[index][0] = log.get("timestamp").toString().substring(1, 11) + "\n";
+                if (!dates.contains(log.get("timestamp").toString().substring(1, 11))) {
+                    dates.add(log.get("timestamp").toString().substring(1, 11)); //Adding the date to the list
                 }
-                else {
-                    results[index][0] = log.get("timestamp").toString().substring(1, 11) + "\t";
-                }
-                index++;
-                dates.add(log.get("timestamp").toString().substring(1, 11));
             }
 
-            index = 1;
-            logIndex = 0;
+            /* Adding the logger to the list */
+            int loggerIndex = 0;
             for (JsonObject log : logs) {
                 if (!rows.contains(log.get("logger").toString().substring(1, log.get("logger").toString().length()-1))) {
-                    if (logIndex+1 == log.size()) {
-                        results[0][index] = log.get("logger").toString().substring(1, log.get("logger").toString().length() - 1) + "\n";
-                    }
-                    else {
-                        results[0][index] = log.get("logger").toString().substring(1, log.get("logger").toString().length() - 1) + "\t";
-                    }
-                    rows.add(log.get("logger").toString().substring(1, log.get("logger").toString().length()-1));
-                    index++;
+                    rows.add(log.get("logger").toString().substring(1, 11)); //Adding the date to the list
+                    loggerIndex++;
                 }
+            }
+
+            /* Adding the level to the list */
+            int levelIndex = loggerIndex;
+            for (JsonObject log : logs) {
+                if (!rows.contains(log.get("level").toString().substring(1, log.get("level").toString().length()-1))) {
+                    rows.add(log.get("level").toString().substring(1, log.get("level").toString().length()-1)); //Adding the date to the list
+                    levelIndex++;
+                }
+            }
+
+            /* Adding the thread to the list */
+            int threadIndex = levelIndex;
+            for (JsonObject log : logs) {
+                if (!rows.contains(log.get("thread").toString().substring(1, log.get("thread").toString().length()-1))) {
+                    rows.add(log.get("thread").toString().substring(1, log.get("thread").toString().length()-1)); //Adding the date to the list
+                    threadIndex++;
+                }
+            }
+
+
+            /* Adding the timestamp to the CSV table */
+            int index = 1;
+            int logIndex = 0; //For determining whether we are at the end of the table
+            for (String date : dates) {
+                if (logIndex+1 == dates.size()) { //We are at the end of the table so add \n
+                    results[index][0] = date + "\n"; //Adding the date to the CSV table
+                }
+                else { //Otherwise add \t
+                    results[index][0] = date + "\t";
+                }
+                index++;
+            }
+
+            /* Adding the logger to the CSV table */
+            index = 1;
+            logIndex = 0;
+            for (String logger : rows) {
+                if (logIndex == loggerIndex) {
+                    results[0][index] = logger + "\n";
+                    break;
+                }
+                else {
+                    results[0][index] = logger + "\t";
+                }
+                index++;
                 logIndex++;
             }
 
+            /* Adding the level to the CSV table */
             logIndex = 0;
-            for (JsonObject log : logs) {
-                if (!rows.contains(log.get("level").toString().substring(1, log.get("level").toString().length()-1))) {
-                    if (logIndex+1 == log.size()) {
-                        results[0][index] = log.get("level").toString().substring(1, log.get("level").toString().length() - 1) + "\n";
-                    }
-                    else {
-                        results[0][index] = log.get("level").toString().substring(1, log.get("level").toString().length() - 1) + "\t";
-                    }
-                    rows.add(log.get("level").toString().substring(1, log.get("level").toString().length()-1));
-                    index++;
+            for (String level : rows) {
+                if (logIndex == levelIndex) {
+                    results[0][index] = level + "\n";
+                    break;
                 }
+                else {
+                    results[0][index] = level + "\t";
+                }
+
+                index++;
             }
 
+            /* Adding the thread to the CSV table */
             logIndex = 0;
-            for (JsonObject log : logs) {
-                if (!rows.contains(log.get("thread").toString().substring(1, log.get("thread").toString().length()-1))) {
-                    if (logIndex+1 == log.size()) {
-                        results[0][index] = log.get("thread").toString().substring(1, log.get("thread").toString().length() - 1) + "\n";
-                    }
-                    else {
-                        results[0][index] = log.get("thread").toString().substring(1, log.get("thread").toString().length() - 1) + "\t";
-                    }
-                    rows.add(log.get("thread").toString().substring(1, log.get("thread").toString().length()-1));
-                    index++;
+            for (String thread : rows) {
+                if (logIndex == threadIndex) {
+                    results[0][index] = thread + "\n";
                 }
+                else {
+                    results[0][index] = thread + "\t";
+                }
+                index++;
+                logIndex++;
             }
 
+            /* Determining CSV table values */
             for (JsonObject log : logs) {
-                int row = rows.indexOf(log.get("logger").toString().substring(1, log.get("logger").toString().length()-1));
-                int col = dates.indexOf(log.get("timestamp").toString().substring(1, 11));
+                /* Calculating the CSV value for the logger */
+                int row = rows.indexOf(log.get("logger").toString().substring(1, log.get("logger").toString().length()-1)); //Getting row index of CSV table based off list
+                int col = dates.indexOf(log.get("timestamp").toString().substring(1, 11)); //Getting col index of CSV table based off list
                 String endChars = "";
+                /* Checking if we are the end of the CSV table */
                 if (col+1 == dates.size()) {
                     endChars = "\n";
                 }
                 else {
                     endChars = "\t";
                 }
+                /* If no value is previously in the cell then the value should be 1. */
                 if (results[col+1][row+1] == null) {
                     results[col+1][row+1] = "1" + endChars;
                 }
+                /* Otherwise it should be old value + 1 */
                 else {
-                    results[col+1][row+1] = Integer.toString(Integer.parseInt(results[col+1][row+1].replaceAll(endChars, "")) + 1) + endChars;
+                    results[col+1][row+1] = (Integer.parseInt(results[col + 1][row + 1].replaceAll(endChars, "")) + 1) + endChars;
                 }
 
+                /* Calculating the CSV value for the level */
                 row = rows.indexOf(log.get("level").toString().substring(1, log.get("level").toString().length()-1));
                 if (results[col+1][row+1] == null) {
                     results[col+1][row+1] = "1" + endChars;
                 }
                 else {
-                    results[col+1][row+1] = Integer.toString(Integer.parseInt(results[col+1][row+1].replaceAll(endChars, "")) + 1) + endChars;
+                    results[col+1][row+1] = (Integer.parseInt(results[col + 1][row + 1].replaceAll(endChars, "")) + 1) + endChars;
                 }
 
+                /* Calculating the CSV value for the thread */
                 row = rows.indexOf(log.get("thread").toString().substring(1, log.get("thread").toString().length()-1));
                 if (results[col+1][row+1] == null) {
                     results[col+1][row+1] = "1" + endChars;
                 }
                 else {
-                    results[col+1][row+1] = Integer.toString(Integer.parseInt(results[col+1][row+1].replaceAll(endChars, "")) + 1) + endChars;
+                    results[col+1][row+1] = (Integer.parseInt(results[col + 1][row + 1].replaceAll(endChars, "")) + 1) + endChars;
                 }
             }
 
+            /* Converting 2d array into String that doesn't include the array formatting (excludes the brackets and the commas) */
             String output = Arrays.stream(results).flatMap(Arrays::stream).collect(Collectors.joining());
             String formattedOutput = output.replaceAll("null", "0");
             writer.append(formattedOutput);
